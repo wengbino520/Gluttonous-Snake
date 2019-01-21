c93693ccb1639c2758d8d86cc763fe011b1f4f62
wengbino520 <997062513@qq.com>
2019-01-21 22:50:39 +0800

    fix the main funtion

--- code/main.c
+++ code/main.c
@@ -13,16 +13,24 @@ int main()
 		case QUIT:
 		{
 			/* quit the game */
+			break;
 		}
 		case START:
 		{
 			/* start the game */
+			break;
 		}
 		case HELP:
 		{
 			/* show info and show how to play */
+			break;
+		}
+		default:
+		{
+			/* show help info */
 		}
 	}
-
+	printf("good work anyway, may you have a good day\n");
+	return 0;
 }
 
